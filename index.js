47b65a30cc7ca09fca50336dcef976c06fe549c8
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-21 22:15:22 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arrayOfDrivers){
+    return arrayOfDrivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(arrayOfDrivers){
+    return arrayOfDrivers.slice(arrayOfDrivers.length-2, arrayOfDrivers.length);
+}
+
+let selectingDrivers = [];
+selectingDrivers.push(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(num){
+    return function(){
+        return Math.pow(num,2);
+    };
+}
+
+function fareDoubler(num){
+    return num*2;
+}
+function fareTripler(num){
+    return num*3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functionToBeExecuted){
+    return functionToBeExecuted(arrayOfDrivers);
+}
